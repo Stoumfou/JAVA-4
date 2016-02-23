ce0a3537bd110d22ef0ff24cd64e3048ac3988a3
Stoumfou <alexis.pambourg@gmail.com>
2016-02-23 14:11:49 +0100

    PieChart

--- src/fr/ece/pambourg/exo3/controller/Controller.java
+++ src/fr/ece/pambourg/exo3/controller/Controller.java
@@ -3,12 +3,23 @@ package fr.ece.pambourg.exo3.controller;
 import fr.ece.pambourg.exo3.model.Model;
 import fr.ece.pambourg.exo3.view.View;
 import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 
 public class Controller {
 	
 	private Model myModel;
-	@FXML private View myView;
+	@FXML private PieChart myPieChart;
 	
+	
+	public void initialize() {
+		
+		Model si= new Model(13, "SI");
+		Model se= new Model(25, "SE");
+		Model ocre= new Model(10, "OCRE");
+		
+		
+		
+	}
 	
 	
 	public Model getMyModel() {
